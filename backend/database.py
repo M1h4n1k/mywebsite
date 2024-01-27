47ee366c6a3f87e9d90ecb086a56bd4500853504
M1h4n1k <mihanik@internet.ru>
2024-01-27 22:49:34 +0200

    docker

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 import motor.motor_asyncio
 import os
 
-client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB_URL", 'mongodb://localhost:27017'))
-db = client.get_database("mywebsite")
-projects = db.get_collection("projects")
-posts = db.get_collection("posts")
+client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv('MONGODB_URL', 'mongodb://localhost:27017'))
+db = client.get_database('mywebsite')
+projects = db.get_collection('projects')
+posts = db.get_collection('posts')
